a195a67e84b820bb4d0712e147661622abb33fad
Chris Lincoln <chris.lincoln12@gmail.com>
2019-05-30 14:38:38 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,7 +5,7 @@ def oxford_comma(array)
     array[-2] << " and "
     array.join
   elsif array.length == 3
-    array.insert(2, " and ")
+    array.insert(2, "and ")
     array.join(", ")
   
   end
